ae35887384f4dbb62d8a8f115654aeebaba6f681
amarpriyesh <63479437+amarpriyesh@users.noreply.github.com>
2022-03-21 17:55:22 -0400

    adding server

--- src/services/users-service.js
+++ src/services/users-service.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "https://priyesh-node-a3.herokuapp.com/api";
+const BASE_URL = "https://priyesh-tuiter-node.herokuapp.com/api";
 // const BASE_URL = "priyesh-node-a3.herokuapp.com/api";
 
 const LOGIN_API = `${BASE_URL}/login`;
